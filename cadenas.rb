b052efc8ad2567bc56505fca61fd727bc2ec243f
dayana <daysoto96@gmail.com>
2025-07-12 21:14:43 -0500

    add

---
+++ cadenas.rb
@@ -0,0 +1,4 @@
+my_name = "Dayana"
+my_surname = "Romero"
+
+puts "Hello my name is #{my_name} and my surname is #{my_surname}"
---
+++ ejercicio1.rb
@@ -0,0 +1,6 @@
+print "ingrese el primer numero:"
+num = gets.to_i
+print "ingrese el segundo numero:"
+num2 = gets.to_i
+puts "la suma es #{num + num2}"
+
